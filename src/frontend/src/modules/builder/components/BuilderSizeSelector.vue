c1e312fff80b895ca488c029bd6385c59552ca24
Nemhis <p.nemphis@gmail.com>
2022-04-10 16:15:04 +0300

    Adding BuilderSizeSelector component

---
+++ src/frontend/src/modules/builder/components/BuilderSizeSelector.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="content__diameter">
+    <div class="sheet">
+      <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+      <div class="sheet__content diameter">
+        <selector-item
+          v-for="size in sizes"
+          :class="`diameter__input diameter__input--${size.code}`"
+          :value="size.id"
+          :label="size.name"
+          :key="size.id"
+          @input="$emit('input', size)"
+          name="diameter"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SelectorItem from "@/modules/ui/components/SelectorItem.vue";
+
+export default {
+  name: "BuilderSizeSelector",
+  components: { SelectorItem },
+  props: {
+    sizes: {
+      type: Array,
+      required: true,
+    },
+    value: {
+      type: Object,
+    },
+  },
+};
+</script>
